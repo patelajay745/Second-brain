dfccc8a72a5a6c7fcbc7a18e8699e1efeecd4f1b
Ajay patel <patel.ajay745@gmail.com>
2024-12-18 13:18:53 -0500

    solved problem in backend

--- backend/dist/routes/auth.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const authcheck_controller_1 = require("../controllers/authcheck.controller");
-const auth_middleware_1 = require("../middlewares/auth.middleware");
-const express_1 = require("express");
-const authRouter = (0, express_1.Router)();
-authRouter.use(auth_middleware_1.verifyJWT);
-authRouter.get("/check", authcheck_controller_1.authcheck);
-exports.default = authRouter;
--- backend/dist/routes/brain.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const brain_controller_1 = require("../controllers/brain.controller");
-const auth_middleware_1 = require("../middlewares/auth.middleware");
-const router = (0, express_1.Router)();
-router.post("/share", auth_middleware_1.verifyJWT, brain_controller_1.toggleShareBrain);
-router.get("/:shareLink", brain_controller_1.getShatedLinkData);
-exports.default = router;
