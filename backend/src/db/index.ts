da4bf7c1158a3037252a4206dd6641a2a723ddf5
Ajay patel <patel.ajay745@gmail.com>
2024-12-18 13:28:31 -0500

    solved problem in backend

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -4,6 +4,9 @@ import { DB_NAME } from "../constants";
 export const connectDB = async () => {
   try {
     const URL = `${process.env.MONGODB_URI}${DB_NAME}`;
+
+    console.log("URl", URL);
+
     const connectionInstance = await mongoose.connect(URL);
 
     console.log("mongoDB connected  ", connectionInstance.connection.name);
