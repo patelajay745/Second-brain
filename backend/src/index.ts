b40149b1cb1de6579beeff4991a03451cd6110d1
Ajay patel <patel.ajay745@gmail.com>
2024-12-18 03:46:59 -0500

    added backend pipeline

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,7 @@ import { app } from "./app";
 import dotenv from "dotenv";
 import { connectDB } from "./db";
 
-dotenv.config({ path: "./.env" });
+// dotenv.config({ path: "./.env" });
 
 const port = 8080;
 
