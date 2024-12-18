2c2d09894b74105738bdf2e972ea9d205d78d899
Ajay patel <patel.ajay745@gmail.com>
2024-12-18 02:05:11 -0500

    added backend pipeline

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,11 +5,11 @@ import cookieparse from "cookie-parser";
 const app: Express = express();
 
 app.use(
-  // cors()
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
+  cors()
+  // cors({
+  //   origin: "http://localhost:5173",
+  //   credentials: true,
+  // })
 );
 
 app.use(
