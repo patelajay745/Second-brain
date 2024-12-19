c2ef018a50331733ff1c1cdff6cd8756b1d07b2a
Ajay patel <patel.ajay745@gmail.com>
2024-12-19 02:49:52 -0500

    fixed frontend

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -4,5 +4,8 @@ export const api = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_BASE_URL,
   timeout: 10000,
   withCredentials: true,
-  headers: { "Content-Type": "application/json" },
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Credentials": "true",
+  },
 });
